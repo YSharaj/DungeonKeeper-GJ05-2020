77383e7e58df48ec9a1a0ca23ab6e4af2970d75e
Dmitry Pyzhov <fiche40@gmail.com>
2020-05-31 12:43:01 +0300

    Rearrange scene order
    Main scene with new level design

--- DungeonKeeper/Assets/Scripts/SceneLoader.cs
+++ DungeonKeeper/Assets/Scripts/SceneLoader.cs
@@ -23,4 +23,8 @@ public class SceneLoader : MonoBehaviour
     public void LoadMainMenu() {
         SceneManager.LoadScene("MainMenu");
 	}
+
+    public void QuitGame() {
+        Application.Quit();
+	}
 }
